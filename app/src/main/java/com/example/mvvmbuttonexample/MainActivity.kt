e09610c6d2ba7a7a1680202e6f293f3cb0aefa49
Krzys <krzycho75000@gmail.com>
2024-08-03 16:45:58 +0200

    theme adjustments

--- app/src/main/java/com/example/mvvmbuttonexample/MainActivity.kt
+++ app/src/main/java/com/example/mvvmbuttonexample/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.mvvmbuttonexample
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.example.mvvmbuttonexample.databinding.ActivityMainBinding
@@ -12,6 +15,10 @@ class MainActivity : AppCompatActivity() {
     private val counterViewModel: CounterViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+//        val controller = WindowInsetsControllerCompat(window, window.decorView)
+//
+//        controller.hide(WindowInsetsCompat.Type.systemBars())
+//        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
